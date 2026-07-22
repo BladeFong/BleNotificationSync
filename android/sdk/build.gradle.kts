8c2dc5a3a10a2a3ee21a5f6e5028c09dd0d8d905
BladeFong <lanefeng1989@163.com>
2026-07-22 16:33:50 +0800

    fix(android): 增加 material-icons-extended 补全 Icon 引用

--- android/sdk/build.gradle.kts
+++ android/sdk/build.gradle.kts
@@ -56,9 +56,11 @@ dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2024.09.00")
     implementation(composeBom)
     implementation("androidx.compose.material:material")
+    implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.activity:activity-compose:1.9.3")
+
 
     // CameraX
     val cameraxVersion = "1.4.1"
